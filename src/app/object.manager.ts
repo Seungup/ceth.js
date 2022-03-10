0c1b4ad88c06c1b6f824bdf30dee84ca0ff01cf0
Seungup Noh <su.noh@naviworks.com>
2022-03-10 13:24:46 +0900

    prettier

--- src/app/object.manager.ts
+++ src/app/object.manager.ts
@@ -1,43 +1,43 @@
-import { Box3, Object3D } from "three";
-import { CT_WGS84, IWGS84 } from "../core";
-import { SingletonWorkerFactory } from "../core/worker-factory";
-import { ObjectAPI } from "./object.api";
+import { Box3, Object3D } from 'three';
+import { CT_WGS84, IWGS84 } from '../core';
+import { SingletonWorkerFactory } from '../core/worker-factory';
+import { ObjectAPI } from './object.api';
 
 export interface RequestResult {
-  objectId: number;
-  result: boolean;
+	objectId: number;
+	result: boolean;
 }
 
 export class ObjectManager {
-  private readonly coreWrapper =
-    SingletonWorkerFactory.getWrapper("CoreThread");
+	private readonly coreWrapper =
+		SingletonWorkerFactory.getWrapper('CoreThread');
 
-  async add(object: Object3D, position?: IWGS84): Promise<ObjectAPI> {
-    const wgs84 = position
-      ? CT_WGS84.fromCesiumWGS84(
-          position.latitude,
-          position.longitude,
-          position.height
-        )
-      : undefined;
+	async add(object: Object3D, position?: IWGS84): Promise<ObjectAPI> {
+		const wgs84 = position
+			? CT_WGS84.fromCesiumWGS84(
+					position.latitude,
+					position.longitude,
+					position.height
+			  )
+			: undefined;
 
-    if (wgs84 && wgs84.height === 0) {
-      wgs84.height = new Box3().setFromObject(object).max.y;
-    }
+		if (wgs84 && wgs84.height === 0) {
+			wgs84.height = new Box3().setFromObject(object).max.y;
+		}
 
-    const id = await this.coreWrapper.add(object.toJSON(), wgs84?.toJSON());
-    return new ObjectAPI(id);
-  }
+		const id = await this.coreWrapper.add(object.toJSON(), wgs84?.toJSON());
+		return new ObjectAPI(id);
+	}
 
-  async get(id: number) {
-    const isExist = await this.coreWrapper.isExist(id);
-    if (isExist) return new ObjectAPI(id);
-  }
+	async get(id: number) {
+		const isExist = await this.coreWrapper.isExist(id);
+		if (isExist) return new ObjectAPI(id);
+	}
 
-  async updateObject(id: number, object: Object3D): Promise<RequestResult> {
-    return {
-      objectId: id,
-      result: await this.coreWrapper.update(id, object.toJSON()),
-    };
-  }
+	async updateObject(id: number, object: Object3D): Promise<RequestResult> {
+		return {
+			objectId: id,
+			result: await this.coreWrapper.update(id, object.toJSON()),
+		};
+	}
 }
