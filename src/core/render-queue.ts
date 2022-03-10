f369da84f610a3c4561158f3c74c2829540712a7
Seungup Noh <su.noh@naviworks.com>
2022-03-10 13:38:08 +0900

    애니메이션 요청시 렌더링하도록 변경

--- src/core/render-queue.ts
+++ src/core/render-queue.ts
@@ -5,40 +5,33 @@ import { Graphic, RenderParam } from './graphic';
  * Cesium 과 three.js 의 스레드 간 렌더 비동기 이슈를 해결합니다.
  */
 export class RenderQueue {
-	private readonly _queue: RenderParam[] = [];
 	private readonly graphic = Graphic.getInstance();
 	private param: RenderParam | undefined;
-	private _handle: number | undefined;
-	private readonly _bindRenderNextAnimationFrame =
-		this._renderNextAnimationFrame.bind(this);
 	private readonly _renderSubject = new Subject<void>();
 	readonly renderNextFrame$ = this._renderSubject.pipe();
 
 	/**
 	 * 다음 장면을 그립니다.
 	 */
-	private _renderNextAnimationFrame() {
-		this.param = this._queue.pop();
+	private _render() {
 		if (this.param) {
 			this.graphic.render(this.param);
+			this.param = undefined;
+			this._renderSubject.next();
 		}
-		this._handle = requestAnimationFrame(
-			this._bindRenderNextAnimationFrame
-		);
-		this._renderSubject.next();
+		this._isRequestRender = false;
 	}
 
+	private _isRequestRender: boolean = false;
 	/**
 	 * 다음 장면을 요청합니다.
 	 * @param param 렌더링 파라미터
 	 */
-	updateNextAnimationFrame(param: RenderParam) {
-		if (this._queue.length) {
-			this._queue[0] = param;
-		} else {
-			this._queue.push(param);
+	requestRender(param: RenderParam) {
+		if (!this._isRequestRender) {
+			this._isRequestRender = true;
+			this.param = param;
+			this._render();
 		}
-
-		if (!this._handle) this._renderNextAnimationFrame();
 	}
 }
